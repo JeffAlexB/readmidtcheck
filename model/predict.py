c7f0ca064f14e864c1081b793054153aab463534
jeffalexb <jeff.alex.burkey@gmail.com>
2025-04-25 18:39:46 +0200

    fine-tuning layout and visualizations for notebook implementation

--- model/predict.py
+++ model/predict.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import joblib
-from utils.preprocessing import preprocess_data
+from utils.preprocessing import preprocess_features
 
 
 def predict_readmission(input_df):
     model = joblib.load('model/readmission_model.pkl')
 
-    X, _ = preprocess_data(input_df)
+    X, _ = preprocess_features(input_df)
 
     predictions = model.predict(X)
     return predictions
